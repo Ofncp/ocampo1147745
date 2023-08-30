fb7ad37674ffa0c9f11db3cfad025370e8e02c66
Ofncp <137748805+Ofncp@users.noreply.github.com>
2023-08-30 15:19:12 +0800

    initial commit
    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+var typed= new Typed(".text", {
+          strings:["Happy Here..", "Sad Leaving..",
+           "Creating Awesomeness..", "Remembering You!"],
+          typeSpeed:100,
+          backSpeed:100,
+          backDelay:1000,
+          loop: true
+    });
+
+
+
+
