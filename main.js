7ca292641eab9cd55c128555aa1a76d2e824e8cd
Ofncp <137748805+Ofncp@users.noreply.github.com>
2023-08-31 10:58:19 +0800

    music Update main.js
    music Update main.js

--- main.js
+++ main.js
@@ -9,4 +9,24 @@ var typed= new Typed(".text", {
 
 
 
+// for music background
 
+
+  const backgroundMusic = document.getElementById("backgroundMusic");
+        const musicToggleButton = document.getElementById("musicToggleButton");
+
+        // Function to toggle music playback and button text
+        function toggleMusic() {
+            if (backgroundMusic.paused) {
+                backgroundMusic.play();
+                musicToggleButton.textContent = "Pause Music";
+            } else {
+                backgroundMusic.pause();
+                musicToggleButton.textContent = "Play Music";
+            }
+        }
+
+        // Auto play music when the page loads
+        window.addEventListener("load", () => {
+            backgroundMusic.play();
+        });
